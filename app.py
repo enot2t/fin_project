92f4fc082f590a59f80ed3e3b7b256d7994eaa85
Valentin Zakharov <valentin_post@list.ru>
2024-04-14 01:31:55 +0300

    update

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ with scol3:
     plt.grid()
     st.pyplot(plt)
 
-st.header("Динамика прироста заработной платы по сравнению с предыдущим годом", anchor=None)
+st.header("динамика изменения реальных зарплат с учетом инфляции", anchor=None)
 
 plt.clf()
 plt.figure(figsize=(12, 8))
